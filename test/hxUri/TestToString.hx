318aaa120c2816361dd1966d3ccd20549eb6df7a
azrafe7 <azrafe7@gmail.com>
2016-03-30 00:27:19 +0200

    some more tests (toString and clone)

--- test/hxUri/TestToString.hx
+++ test/hxUri/TestToString.hx
@@ -13,5 +13,20 @@ class TestToString {
     public function testToString() {
 		var uri = new Uri("http://www.example.com/foo/bar?page=1#baz");
 		Assert.equals("http://www.example.com/foo/bar?page=1#baz", uri.toString());
+		uri = new Uri("?page");
+		Assert.equals("?page", uri.toString());
+		uri = new Uri("");
+		Assert.equals("", uri.toString());
     }
+	
+	public function testClone() {
+		var u = new Uri("?foo");
+		var clone = u.clone();
+		Assert.equals(u.toString(), clone.toString());
+		Assert.notEquals(u, clone);
+		u = new Uri("http://www.example.com/foo/bar?page=1#baz");
+		clone = u.clone();
+		Assert.equals(u.toString(), clone.toString());
+		Assert.notEquals(u, clone);
+	}
 }
